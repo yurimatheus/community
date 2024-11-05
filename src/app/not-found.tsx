2da54b9c7164416f65a309e3431ef18eed33a162
Yuri Matheus Gomes <y.matheusdesign@gmail.com>
2024-11-05 20:58:11 -0300

    Atualização no código do texto

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -36,7 +36,7 @@ export default function NotFound() {
 
         <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</h1>
 
-        <p className="mt-4 text-gray-500">We can't find that page.</p>
+        <p className="mt-4 text-gray-500">We can&#39;t  find that page.</p>
       </div>
     </div>
   )
