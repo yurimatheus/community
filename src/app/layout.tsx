74ffe22ae777a165698f6b7c14ef3e1a7e90137d
Yuri Matheus Gomes <y.matheusdesign@gmail.com>
2024-11-05 20:29:24 -0300

    Exclusão das fontes internas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "MyLnkd Community",
@@ -24,7 +12,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // className={`${geistSans.variable} ${geistMono.variable} antialiased`} >> dentro do body
     <html lang="en">
       <body>
         {children}
