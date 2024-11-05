c87eeb5bdb30bfa9c7f67b334a5dab960fc5e50d
Yuri Matheus Gomes <y.matheusdesign@gmail.com>
2024-11-05 20:42:05 -0300

    Nova estrutura do dashboard

--- src/app/(app)/dashboard.tsx
+++ src/app/(app)/dashboard.tsx
@@ -1,6 +1,12 @@
+import { Sidemenu } from "@/components/(app)/Sidemenu";
+
 export function Dashboard() {
   return (
     <>
+      <Sidemenu />
+      <section className="ps-8 pt-8">
+        container
+      </section>
     </>
   )
 }
