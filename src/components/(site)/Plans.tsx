16a07f0031657560c851344eeef4cbda10d0f78a
Yuri Matheus Gomes <y.matheusdesign@gmail.com>
2024-11-05 20:25:20 -0300

    Adição de margin-auto horizontal

--- src/components/(site)/Plans.tsx
+++ src/components/(site)/Plans.tsx
@@ -1,6 +1,6 @@
 export function SitePlans() {
   return (
-    <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+    <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-stretch md:grid-cols-3 md:gap-8">
         <div className="divide-y divide-gray-200 rounded-2xl border border-gray-200 shadow-sm">
           <div className="p-6 sm:px-8">
